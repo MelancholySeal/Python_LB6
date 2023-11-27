0e3674cb1c64e1f6ec44d5a527fde84d40db2557
MelancholySeal <kirillazimoff@yandex.ru>
2023-11-27 16:48:06 +0300

    update

--- pythonProject/tasks/htask.py
+++ pythonProject/tasks/htask.py
@@ -1,12 +1,12 @@
 word1 = 'abbcc'
 word2 = 'abbvv'
-nullword=''
+nullword = ''
 for i in range(len(word1)):
     if word1[i] not in word2:
-        nullword+=word1[i]+' '
+        nullword += word1[i]+' '
 
 for i in range(len(word2)):
     if word2[i] not in word1:
-        nullword+=word2[i]+' '
+        nullword += word2[i]+' '
 
 print(nullword)
--- pythonProject/tasks/task3.py
+++ pythonProject/tasks/task3.py
@@ -1,20 +1,20 @@
 import re
 word = input("Введите слово")
-k=int(input('Введите номер буквы'))
-data=[]
-data2=[]
-if len(word)<3:
+k = int(input('Введите номер буквы'))
+data = []
+data2 = []
+if len(word) < 3:
     print('Слово слишком маленькое')
 else:
     for i in range(len(word)):data.append(word[i])
-    if k==0:
-        data[1]='§'
+    if k == 0:
+        data[1] = '§'
     else:
-        data[k-1]='§'
-        data[1]='§'
+        data[k-1] = '§'
+        data[1] = '§'
     for i in range(len(data)):
-        if data[i]!='§':
+        if data[i] != '§':
             data2.append(data[i])
-data= ''.join(data2)
+data = ''.join(data2)
 
 print(data)
