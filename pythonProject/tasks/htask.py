6e3e159b5855cd7332cbb12a432211253e85987b
MelancholySeal <kirillazimoff@yandex.ru>
2023-11-30 15:24:59 +0300

    Update htask.py

--- pythonProject/tasks/htask.py
+++ pythonProject/tasks/htask.py
@@ -6,12 +6,14 @@ if __name__ == '__main__':
     word1 = input("Введите первое слово:")
     word2 = input("Введите второе слово:")
     nullword = ''
-    for i in range(len(word1)):
-        if word1[i] not in word2:
-            nullword += word1[i]+' '
+    for i in enumerate(word1):
+        char = i[0]
+        if word1[char] not in word2:
+            nullword += word1[char]+' '
 
-    for i in range(len(word2)):
-        if word2[i] not in word1:
-            nullword += word2[i]+' '
+    for i in enumerate(word2):
+        char = i[0]
+        if word2[char] not in word1:
+            nullword += word2[char]+' '
 
     print(nullword)
